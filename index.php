8bb7f4aac706c0d1acb80130f93c8274e79cb686
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-07 10:02:35 -0300

    subindo index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "olá mundo";
+?>
+<html>
+    <head></head>
+    <meta charset="utf-8"/>
+    <body>
+        Enedson Gomes - curso <HTML>
+        Enedson Gomes 1
+        Enedson Gomes 2
+        Enedson Gomes 3        
+    </body>
+</html>
