f57dc5c2068d238914b6fb718dc794c7f8935c3f
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-08 00:56:48 -0300

    subindo pasta

---
+++ php/comentarios-php/index.php
@@ -0,0 +1,20 @@
+<?php
+// echo "comentar uma única linha de código";
+//
+# echo "comentar uma única linha de código;
+#
+?>
+
+<?php
+/* echo "comentar várias linhas de código;
+   echo "comentar várias linhas de código;
+*/
+?>
+
+<?php
+/* Sempre comente seu código;
+   Com o tempo podemos esquecer para que serve;
+   Facilita o entendimento de terceiros;
+*/
+?>
+
