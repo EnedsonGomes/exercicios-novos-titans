bdb749ede6d631e39811f5187b342a612bd95490
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-09 19:58:48 -0300

    corrigindo index.php

---
+++ php/primeiro-echo/index1.php
@@ -0,0 +1,6 @@
+<?php
+    //Minhas primeira linhas de PHP! Isso ae!
+    echo "<h2 algn= 'center'>
+                  O meu programa está ecoando corretamente no meu servidor PHP!
+          </h2>";
+?>
