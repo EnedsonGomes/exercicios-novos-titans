76de87eeec56abe1c3e1219c370aefbe462319c6
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-09 20:48:51 -0300

    exemplos de combinação

---
+++ php/variaveis/index.php
@@ -0,0 +1,20 @@
+<?php
+    // Define o nome da variável
+    $variavel = "nome";
+?>
+
+<?php
+    /* Nunca inicie a nomeclaruta de variáveis com números
+       Nunca utilize espaços no meio do identificador da variável
+       Nunca utiliza caracteres especiais:
+       É case-sensitive, ou seja, distingue maiúsculas de minúsculas. cuidado!
+    */
+?>
+
+<?php
+    // Boas práticas
+    # evite criar variáveis com mais de 15 caracteres para clareza do código
+    # nomes das variáveis devem ser signigcativos e transmitir a idéia de seu conteúdo
+    # Utilize preferencialmente palavras em minúsculo (separadas por _ )
+?>
+
