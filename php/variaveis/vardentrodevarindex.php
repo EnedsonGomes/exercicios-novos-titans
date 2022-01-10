d1a41813003d0dc3e649542465e423ed7bafe34b
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-10 11:51:02 -0300

    corrigindo digitação data_hoje

--- php/variaveis/vardentrodevarindex.php
+++ php/variaveis/vardentrodevarindex.php
@@ -6,7 +6,7 @@
                   $idade = 400;
 
                   $frase_capital = "A cidade de $cidade é a capital do $estado";
-                  $frase_idade = "%cidade tem mais de $idade anos";
+                  $frase_idade = "cidade tem mais de $idade anos";
 
                   echo "<h3>$frase_capital</h3>";
                   echo "<h4>$frase_idade</h4>";
