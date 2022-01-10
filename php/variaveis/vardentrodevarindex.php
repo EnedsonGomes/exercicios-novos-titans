f807d68881d927e76beef3dfb6dfba22634689cb
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-09 21:05:46 -0300

    exemplo de variaveis dentro de variaveis

---
+++ php/variaveis/vardentrodevarindex.php
@@ -0,0 +1,16 @@
+<html>
+      <body>
+             <?php
+                  $cidade = "Natal";
+                  $estado = "RN";
+                  $idade = 400;
+
+                  $frase_capital = "A cidade de $cidade é a capital do $estado";
+                  $frase_idade = "%cidade tem mais de $idade anos";
+
+                  echo "<h3>$frase_capital</h3>";
+                  echo "<h4>$frase_idade</h4>";
+                ?>
+        </body>
+</html>
+            
