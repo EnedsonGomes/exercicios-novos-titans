8c56aae6b7645bac817f58c36b2e4151e9adafa8
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-10 21:24:59 -0300

    teste de variaveis

---
+++ php/testevariaveis/index.php
@@ -0,0 +1,18 @@
+<?php
+     $prova1 = 7;
+     $prova2 = 4;
+     $nota = ($prova1 + $prova2) / 2;
+     
+     if($nota<3)
+             $desempenho = "PÉSSIMO";
+     elseif($nota<5)
+             $desempenho = "RUIM";
+     elseif($nota<7)
+             $desempenho = "MÉDIO";
+     elseif($nota<8)
+             $desempenho = "BOM";
+     else
+             $desempenho = "EXCELENTE";
+     echo "O seu desempenho foi $desempenho";
+?>
+
