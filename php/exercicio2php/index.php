29aa2a2b9acf316ba071959ccdbecde84440fe72
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-18 10:41:28 -0300

    subindo nova atividde

---
+++ php/exercicio2php/index.php
@@ -0,0 +1,33 @@
+<?Php
+//Entrar com um número 
+//Informar se ele é divisível por 10, por 5, por 2 
+//Ou se não é divisível por nenhum destes
+
+     $value = $_POST['n1'];
+      
+           
+     if ($_POST['n1']/10 == 0);
+    {
+         echo $_POST['n1'] . "é divisível por 10" . $_POST['n1']/10;}
+
+    else
+    {   
+        echo $_POST['n1'] . "não divisível por 10";}
+
+
+        ($_POST['n1']/5 == 0);
+        ($_POST['n1']/2 == 0);
+
+        echo 'Sim'
+
+
+     else{
+        ($_POST['n1']/10  0);
+        ($_POST['n1']/5 == 0);
+        ($_POST['n1']/2 == 0);
+     }
+
+     echo 'o resultado da soma é ' . $sum . "\n";
+ 
+
+
