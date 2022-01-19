d5229607553d6b14fcc1ebfe6981a4cd722d6624
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-19 00:50:59 -0300

    código exercicio2

--- php/exercicio2php/index.php
+++ php/exercicio2php/index.php
@@ -1,33 +1,46 @@
-<?Php
+<?php
 //Entrar com um número 
 //Informar se ele é divisível por 10, por 5, por 2 
 //Ou se não é divisível por nenhum destes
 
-     $value = $_POST['n1'];
-      
-           
-     if ($_POST['n1']/10 == 0);
-    {
-         echo $_POST['n1'] . "é divisível por 10" . $_POST['n1']/10;}
+//$value = $_POST['n1'];
+$value = $_POST['n1'] ;
 
-    else
-    {   
-        echo $_POST['n1'] . "não divisível por 10";}
+if ($value % 10 == 0){
+   echo $value . " é divisível por 10 <br> ";
+}
+else {   
+   echo $value . "não divisível por 10 <br> ";
+}
+
+if ($value % 5 == 0){
+   echo $value . " é divisível por 5 <br> ";
+}
+else {   
+   echo $value . "não divisível por 5 <br> ";
+}
+
+if ($value % 2 == 0){
+   echo $value . " é divisível por 2 <br> ";
+}
+else {   
+   echo $value . "não divisível por 2 <br> ";
+}
 
 
-        ($_POST['n1']/5 == 0);
-        ($_POST['n1']/2 == 0);
+        //($_POST['n1']/5 == 0);
+        //($_POST['n1']/2 == 0);
 
-        echo 'Sim'
+        //echo 'Sim';
 
 
-     else{
-        ($_POST['n1']/10  0);
-        ($_POST['n1']/5 == 0);
-        ($_POST['n1']/2 == 0);
-     }
+     //else{
+        //($_POST['n1']/10  0);
+        //($_POST['n1']/5 == 0);
+        //($_POST['n1']/2 == 0);
+     
 
-     echo 'o resultado da soma é ' . $sum . "\n";
+     //echo 'o resultado da soma é ' . $sum . "\n";
  
 
 
