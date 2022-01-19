30abfba79d74445138eafc5888c5248f5961c617
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-19 11:29:38 -0300

    correção soma

--- exercicio1back.php
+++ exercicio1back.php
@@ -6,13 +6,13 @@
      $sum = $_POST['n1'] + $_POST['n2'];
          
      if ($sum > 20) {
-         $sum = $sum + 8;
-         $sum += 8;  
+         $sum = $sum + 8; 
+         echo 'o resultado da soma é ' . $sum . "\n"; 
      }
      else{
          $sum -= 5;
-     }
-
      echo 'o resultado da soma é ' . $sum . "\n";
+     }
+     
  
  ?>
