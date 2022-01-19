87c6e0c63760af7f4978ec229cf68a0eb7e73aca
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-19 11:30:42 -0300

    corrigindo soma

--- exercicio1front.php
+++ exercicio1front.php
@@ -19,22 +19,21 @@
             
 <?php
 
-  $value1 = $_POST['n1'];
-  $value2 = $_POST['n2'];
+     $value1 = $_POST['n1'];
+     $value2 = $_POST['n2'];
  
-  $sum = $_POST['n1'] + $_POST['n2'];
+     $sum = $_POST['n1'] + $_POST['n2'];
          
-  if ($sum > 20) {
-    $sum = $sum + 8;
-    $sum += 8;  
-}
-  else{
-    $sum -= 5;
-}  
-
-  echo 'o resultado da soma é ' . $sum . "\n";
- 
-?>
+     if ($sum > 20) {
+         $sum = $sum + 8; 
+         echo 'o resultado da soma é ' . $sum . "\n"; 
+     }
+     else{
+         $sum -= 5;
+     echo 'o resultado da soma é ' . $sum . "\n";
+     }
+     
+ ?>
 
 </body>    
 </html>
