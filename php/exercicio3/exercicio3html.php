ee8752e40c0a8d426c23e990cb1357e62320ee94
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-22 22:05:50 -0300

    html e css

---
+++ php/exercicio3/exercicio3html.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+     <meta charset="utf-8"/>
+     <title>Exercicio3php</title> 
+     <link rel="stylesheet" type="text/css" href="exercicio3.css">
+</head>
+<body>
+<form action = "index.php" method = "POST">
+<fieldset id="fundo">
+<div id="title"><h3>Cadastro Pessoal<br></h3>
+</div>
+    <p>              
+     <input id="name" name="name1" type="text" placeholder="Digite seu nome..."/>
+     <p> 
+     <input id="sexo" name="sexo1" type="text" placeholder="Digite seu sexo..."/>
+     <p> 
+     <input id="age" name="age1" type="number" placeholder="Digite sua idade..."/>
+    </p>  
+     <input id="enviar" class="btn_submit" type="submit" value="Enviar">
+    <br>
+</fieldset>            
+</form>
