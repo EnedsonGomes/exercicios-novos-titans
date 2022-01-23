c548ffa0e402604e865afcb79be15c3b4e17bb4f
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-22 22:05:50 -0300

    html e css

---
+++ php/exercicio3/index.php
@@ -0,0 +1,11 @@
+<?php
+/*Entrar com nome, sexo e idade de uma
+pessoa. 
+Se a pessoa for do sexo feminino e
+tiver menos que 25 anos, imprimir nome e a
+a mensagem: ACEITA. 
+Caso contrário,
+imprimir nome e a mensagem: NÃO ACEITA.
+*/
+
+
