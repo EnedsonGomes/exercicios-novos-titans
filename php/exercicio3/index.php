8555c1b02751bb2a412614984f78fc653eed2d9f
Enedson Gomes <enedsonoriundo@gmail.com>
2022-01-25 17:02:29 -0300

    exercicio3

--- php/exercicio3/index.php
+++ php/exercicio3/index.php
@@ -1,19 +1,66 @@
-<?php
-/*Entrar com nome, sexo e idade de uma
+
+<!--Entrar com nome, sexo e idade de uma
 pessoa. 
 Se a pessoa for do sexo feminino e
 tiver menos que 25 anos, imprimir nome e a
 a mensagem: ACEITA. 
 Caso contrário,
 imprimir nome e a mensagem: NÃO ACEITA.
-*/
+-->
 
+<html>
+<head>
+     <meta charset="utf-8"/>
+     <title>Exercicio3php</title> 
+     <link rel="stylesheet" type="text/css" href="exercicio3.css">
+</head>
+<body>
+<form action = "index.php" method = "POST">
+<fieldset id="fundo">
+<div id="title"><h3>Cadastro Pessoal<br></h3>
+</div>
+    <p>              
+     <input class="digite" name="name1" type="text" placeholder="Digite seu nome..."/>
+    </p>        
+<div class="middle">
+    <p>
+     <label>Masculino</label>    
+     <input name="sexo" type="radio" value="M"/>
+    </p> 
+    <p>
+     <label>Feminino</label>
+     <input name="sexo"type="radio" value="F"/>
+    </p> 
+    <p>
+     <label>Outros</label>
+     <input name="sexo"type="radio" value="O"/> 
+   </p> 
+</div>
+   <p>
+     <input class="digite" name="age1" type="number" placeholder="Digite sua idade..."/>
+    </p>  
+     <input id="enviar" class="btn_submit" type="submit" value="Enviar">
+    <br>
+
+</fieldset>            
+</form>
+<?php
 $name = $_POST["name1"];
-$sexo = $_POST["sexo1"];
+$sexo = $_POST["sexo"];
 $age = $_POST["age1"];
 
-if ($value3 < 25);
+if($name == null && $sexo == null && $age== null){
+    echo ""; 
+}
+elseif($age <= 25 && $sexo === "F"){
+        echo "<b>ACEITA</b>";
+        }
+        else{
+             echo "<div class='digite'>NAO ACEITA</div>";
+        }
+?>
+</body>
+</html>
 
-elseif($value2 == 2){ 
-    echo "ACEITA <br>";
- }
+
+
