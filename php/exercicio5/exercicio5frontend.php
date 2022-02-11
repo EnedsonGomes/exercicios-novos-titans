cdcba27a851919d1c32bfc1e2289d8c9a7163ae9
Enedson Gomes <enedsonoriundo@gmail.com>
2022-02-10 22:42:07 -0300

    Corrigindo a palavra number no formulário, corrigindo fonte css formulário e corrigindo titulos do formulário

--- php/exercicio5/exercicio5frontend.php
+++ php/exercicio5/exercicio5frontend.php
@@ -11,10 +11,13 @@ acordo com tamanho dos lados.
 <body>
    <form action = "index.php" method = "POST">
     <fieldset class="fundo">
-    <div class="title"><h3>Medidas do Triangulo</h3><br></div>
-    <div><input class="digite" name="n1" type="number" placeholder="Digite a medida..."/></div>     
-    <div><input class="digite" name="n2" type="umber" placeholder="Digite a medida..."/></div>
+    <div id="title"><h3>Medidas do Triangulo</h3><br></div>
+    <div><input class="digite" name="n1" type="number" placeholder="Digite a medida..."/></div> 
+    <br>        
+    <div><input class="digite" name="n2" type="number" placeholder="Digite o numero..."/></div>
+    </br>
     <div><input class="digite" name="n3" type="number" placeholder="Digite a medida..."/></div>
+    <br>
     <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
     </fieldset>            
 </form>
