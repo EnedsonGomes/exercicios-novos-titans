50ce3f6a0d34551c2c978c2c09cc5c1e256a2254
Enedson Gomes <enedsonoriundo@gmail.com>
2022-02-13 17:48:54 -0300

    frontand exercicio6

---
+++ php/exercicio6/exercicio6frontend.php
@@ -0,0 +1,27 @@
+<!--Ler um número inteiro entre 1 e 12 e escrever
+o mês correspondente. Caso o número seja
+fora desse intervalo, informar que não existe
+mês com este número
+-->
+
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Exercicio6php</title> 
+         <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+    <form action = "index.php" method = "POST">  
+        <fieldset class="contorno"> 
+    <div id="title"><h1>Mês Correspondente</h1></div>
+    <fieldset class="fundo">
+        <div><input class="digite" name="n1" type="number" placeholder="Mês de nascimento..."/></div> 
+    </fieldset>
+        <br>
+        <br>
+        <input class="enviar" type="submit" value="Enviar">   
+        </p>
+        </fieldset>            
+    </form>
+    </body>
+    </html>
