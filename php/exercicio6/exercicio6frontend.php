d0e969180f76dbf3ab86cdaaf389fe8301c7199d
Enedson Gomes <enedsonoriundo@gmail.com>
2022-02-13 19:47:50 -0300

    corrigindo identação

--- php/exercicio6/exercicio6frontend.php
+++ php/exercicio6/exercicio6frontend.php
@@ -5,23 +5,23 @@ mês com este número
 -->
 
 <html>
-    <head>
-         <meta charset="utf-8"/>
-         <title>Exercicio6php</title> 
-         <link rel="stylesheet" type="text/css" href="estilo.css">
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8"/>
+    <title>Exercicio6php</title> 
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
     <form action = "index.php" method = "POST">  
         <fieldset class="contorno"> 
-    <div id="title"><h1>Mês Correspondente</h1></div>
-    <fieldset class="fundo">
-        <div><input class="digite" name="n1" type="number" placeholder="Mês de nascimento..."/></div> 
-    </fieldset>
-        <br>
-        <br>
-        <input class="enviar" type="submit" value="Enviar">   
-        </p>
+            <div id="title"><h1>Mês Correspondente</h1></div>
+            <fieldset class="fundo">
+                <div><input class="digite" name="n1" type="number" placeholder="Mês de nascimento..."/></div> 
+            </fieldset>
+            <br>
+            <br>
+            <input class="enviar" type="submit" value="Enviar">   
+            <br>
         </fieldset>            
     </form>
-    </body>
-    </html>
+</body>
+</html>
