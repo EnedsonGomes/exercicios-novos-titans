254cc29dcdb314b6db77cc612ea03a8c6f8e7e3c
Enedson Gomes <enedsonoriundo@gmail.com>
2022-02-27 20:48:58 -0300

    formulário html e estilo css exercicio8frontend

---
+++ php/exercicio8/exercicio8frontend.php
@@ -0,0 +1,24 @@
+<!--Criar um algoritmos que leia um número e
+imprima todos os número de 1 até ele e o seu
+produto.
+-->
+
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Exercicio8php</title> 
+         <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+    <div class="enunciado">Entrar com um número, enviar e aguardar a impressão a partir de 01 até ele, e o seu
+produto. </div>
+    <form action = "index.php" method = "POST">
+    <fieldset class="fundo">
+        <div id="title"><h3>Checagem de Produto</h3><br></div>
+        <div><input class="digite" name="n3" type="number" placeholder="Digite o numero..."/></div>
+        </div>  
+        <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
+    </fieldset>            
+    </form>
+    </body>
+    </html>
