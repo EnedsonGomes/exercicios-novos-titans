50c427473b9e57f3520c09e99aa9f0587b718ebd
Enedson Gomes <enedsonoriundo@gmail.com>
2022-03-02 21:07:12 -0300

    frontend exercicio 9 e estilo css do formulário

---
+++ php/exercicio9/exercicio9frontend.php
@@ -0,0 +1,21 @@
+<!--Entrar com o número de vezes que se deseja
+imprimir a palavra SOL e imprimir
+-->
+
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Exercicio9php</title> 
+         <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+    <div class="enunciado">Entrar com número de vezes que deseja imprimir a palavra Sol </div>
+    <form action = "index.php" method = "POST">
+    <fieldset class="fundo">
+        <div id="title"><h3>Número de Impressão</h3><br></div>
+        <div><input class="digite" name="n3" type="number" placeholder="Digite o numero..."/></div>
+        <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
+    </fieldset>            
+    </form>
+    </body>
+    </html>
