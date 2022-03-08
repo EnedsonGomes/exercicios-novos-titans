ccb75a263d42e9dec8cf7107bfc5e0ba9bf08dcd
Enedson Gomes <enedsonoriundo@gmail.com>
2022-03-08 19:34:36 -0300

    codigo php mais html mais estilo.css

--- php/exercicio7/index.php
+++ php/exercicio7/index.php
@@ -6,39 +6,30 @@
     </head>
     <body>
     <div class="enunciado"> Digite o nome do livro que será emprestado e selecione o usuário. </div>
-    <fieldset class="fundo">
+ <fieldset class="fundo">
     <div class="title">Controle de Biblioteca</div>
+    <form action = "index.php" method = "POST">
+        <div>
+            <label for="nomedolivro" class="nomedolivro"></label>
+            <input required class="input" name="nomedolivro" type="texto" placeholder="Digite o Nome do Livro..."/>
+        </div>    
+        <select value="" placeholder="selecione" required name="TipodeUsuario" class="selecionar">
+            <option></option>
+            <option value="Professor">Professor</option>
+            <option value="Aluno">Aluno</option>
+        </select>   
+        <input type="submit" class="button" value="Salvar Dados">
+    </form>
+</fieldset> 
 <?php
-$imput1 = $_POST["nomedolivro"];
-$imput2 = $_POST["TipodeUsuário"];
-
-if (isset($_POST["nomedolivro"]) && isset($_POST["TipodeUsuário"])){
-
-    if($imput2===Professor){
-        echo "Você tem 10 dias a partir de hoje para devolver o livro:" . $imput1;
+$bookName = $_POST["nomedolivro"];
+$userType = $_POST["TipodeUsuario"];
+    if($userType=="Professor"){
+        echo "Você tem 10 dias a partir de hoje para devolver o livro:" . $bookName;
     }
-    elseif($imput2===Aluno){
-        echo "Você tem dias a partir de hoje para devolver o livro:" . $imput1;
+    elseif($userType=="Aluno"){
+        echo "Você 03 tem dias a partir de hoje para devolver o livro:" . $bookName;
     }
-        unset ($_POST["nomedolivro"],$_POST["TipodeUsuário"]);
-}
-else {
-   ?>
-   <form action = "index.php" method = "POST">
-            <div class="nomedolivro">Livro:</div>
-            <input class="input" type="texto" placeholder="Digite o Nome do Livro..."/>
-            <div class="TipodeUsuário">Usuário:</div>
-            <select class="selecionar">
-                <option></option>
-                <option>Professor</option>
-                <option>Aluno</option>
-            </select>            
-            <input type="submit" class="button" value="Salvar Dados">
-        </form>
-    </fieldset>   
-</form>
-<?php
-}
 ?>
 </body>
-</html>    
+</html>   
