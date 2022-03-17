73fdcb0e148705c9cc525c43987f28aa772a506c
Enedson Gomes <enedsonoriundo@gmail.com>
2022-03-17 20:32:27 -0300

    subindo html e css do formulário exercicio10

---
+++ php/exercicio10/exercicio10frontend.php
@@ -0,0 +1,63 @@
+<!--Entrar com 20 números e imprimir a soma
+dos positivos e o total de números negativos
+-->
+
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Exercicio10php</title> 
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<div class="enunciado"> Prencher 20 numeros e submeter ao código</div>
+
+
+   <form action = "index.php" method = "POST">
+    <fieldset class="fundo">
+    <div id="title"><h3>Seleção de Números</h3><br></div>
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br>        
+    <div><input class="digite" name="n2" type="number" placeholder="Digite o numero..."/></div>
+    </br>
+    <div><input class="digite" name="n3" type="number" placeholder="Digite o numero..."/></div>
+    <br>
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+    <div><input class="digite" name="n1" type="number" placeholder="Digite o numero..."/></div> 
+    <br> 
+
+    <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
+    </br>
+        </fieldset>            
+</form>
+</body>
+</html>  
