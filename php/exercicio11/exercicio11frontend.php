c5d7966269c97ef27695d9e22a9c253b84263b59
Enedson Gomes <enedsonoriundo@gmail.com>
2022-03-23 00:39:52 -0300

    formulário html e estilo.css do formuário

---
+++ php/exercicio11/exercicio11frontend.php
@@ -0,0 +1,21 @@
+<!--Criar um algoritmos que imprima a tabuada
+de um número
+-->
+
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Exercicio11php</title> 
+         <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+    <div class="enunciado">Digite um numero para ver a tabuada. </div>
+    <form action = "index.php" method = "POST">
+    <fieldset class="fundo">
+        <div id="title"><h3>Tabuada</h3><br></div>
+        <div><input class="digite" name="n3" type="number" placeholder="Digite o numero..."/></div>
+        <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
+    </fieldset>            
+    </form>
+    </body>
+    </html>
