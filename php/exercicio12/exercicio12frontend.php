4edfe0c54ef16d928e5d8b9494e5464363e8a53c
Enedson Gomes <enedsonoriundo@gmail.com>
2022-05-18 09:58:37 -0300

    Frontend exercicio12 finalizado

---
+++ php/exercicio12/exercicio12frontend.php
@@ -0,0 +1,26 @@
+<!--Criar um algoritmos que entre com uma
+palavra e imprima conforme o exemplo
+◦ Palavra: sonho
+◦ SONHO
+◦ SONHO SONHO
+◦ SONHO SONHO SONHO
+◦ SONHO SONHO SONHO SONHO
+-->
+
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Exercicio12php</title> 
+         <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+    <div class="enunciado">Exercicio 12</div>
+    <form action = "index.php" method = "POST">
+    <fieldset class="fundo">
+        <div id="title"><h3>Digite uma palavra</h3><br></div>
+        <div><input class="digite" name="n3" type="text" placeholder="Digite aqui..."/></div>
+        <input class="enviar" class="btn_submit" type="submit" value="Enviar">   
+    </fieldset>            
+    </form>
+    </body>
+    </html>
