5f552df96fbd1d74b0677f0338fecc1ece4271e8
Enedson Gomes <enedsonoriundo@gmail.com>
2022-05-19 14:58:05 -0300

    indice do exercicio5

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-<?php
-echo "olá mundo";
-?>
-<html>
-    <head></head>
-    <meta charset="utf-8"/>
-    <body>
-        Enedson Gomes - curso <HTML>
-        Enedson Gomes 1
-        Enedson Gomes 2
-        Enedson Gomes 3        
-    </body>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Índice</title>
+</head>
+<body>
+<h1>Índice</h1>
+<a href=php/exercicio1>Exercício 1</a><br/>
+<a href=php/exercicio2>Exercício 2</a><br/>
+<a href=php/exercicio3>Exercício 3</a><br/>
+<a href=php/exercicio4>Exercício 4</a><br/>
+<a href=php/exercicio5/estilo.css/index.php>Exercício 5</a><br/>
+<a href=php/exercicio6>Exercício 6</a><br/>
+<a href=php/exercicio7>Exercício 7</a><br/>
+<a href=php/exercicio8>Exercício 8</a><br/>
+<a href=php/exercicio9>Exercício 9</a><br/>
+<a href=php/exercicio10>Exercício 10</a><br/>
+<a href=php/exercicio11>Exercício 11</a><br/>
+<a href=php/exercicio12>Exercício 12</a><br/>
+<a href=php/exercicio13>Exercício 13</a><br/>
+<a href=php/exercicio14>Exercício 14</a><br/>
+<a href=php/exercicio15>Exercício 15</a><br/>
+<a href=php/exercicio16>Exercício 16</a><br/>
+<a href=php/exercicio17>Exercício 17</a><br/>
+<a href=php/exercicio18>Exercício 18</a><br/>
+<a href=php/exercicio19>Exercício 19</a><br/>
+<a href=php/exercicio20>Exercício 20</a><br/>
+<a href=/28-crud>Aulas 46 47 48 e 49</a><br/>
+</body>
 </html>
+
